d2c911ea68a234f51bd1d3659bcf2df9b33bb43e
SallyNader <SALLY.NADER.AHMED@hotmail.com>
2018-02-10 09:39:34 +0200

    make authentication demo using jwt

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var jwtRoute = require('./routes/jwt');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -24,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api',jwtRoute);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +43,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
