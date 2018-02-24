1748c2869b0441e67e54c0a10ce5107e29a7b3da
Hesham Adel <heshamscorpe55@gmail.com>
2018-02-24 08:04:08 +0200

    Update eslint and lint line ending

--- routes/jwt.js
+++ routes/jwt.js
@@ -1,49 +1,52 @@
 const express = require('express'),
-      router = express.Router(),
-      jwt = require('jsonwebtoken');
+  router = express.Router(),
+  jwt = require('jsonwebtoken');
 
- //protect route with verify token middleware
+//protect route with verify token middleware
 router.post('/post', verifyToken, (req, res) => {
   jwt.verify(req.token, 'secretKey', (err, authData) => {
     if(err) {
       res.sendStatus(403);
     } else {
       res.json({
-        message: "post it",
+        message: 'post it',
         authData
       });
     }
-  });    
+  });
 });
 
 router.post('/login', (req, res) => {
   const user = {
     id: 1,
-    username: "sally",
-    email: "sally@gmail.com"
+    username: 'sally',
+    email: 'sally@gmail.com'
   }
 
   jwt.sign({user: user}, 'secretKey', { expiresIn: '30s'}, (err, token) => {
     res.json({
       token: token
     });
-  });    
+  });
 });
 
 function verifyToken(req, res, next) {
   //get auth header value
-  const bearerHeader = req.headers['authorization'];
+  const bearerHeader = req.headers.authorization;
   //check if bearer is undefined
+
   if(typeof bearerHeader !== 'undefined') {
-    const bearer = bearerHeader.split(" ");
+    const bearer = bearerHeader.split(' ');
+
     //get token from array
     const bearerToken = bearer[1];
     //set the token
+
     req.token = bearerToken;
     //next middleware
     next();
   } else {
-    //forbidden 
+    //forbidden
     res.sendStatus(403);
   }
 }
